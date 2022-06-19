98537ac88534e8f29270d1253f472d479a40a452
SonmezYigithan <sonmez.yigithan@gmail.com>
2022-06-19 18:23:33 +0300

    initial

---
+++ app/src/main/java/com/yigithansonmez/homework3/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.yigithansonmez.homework3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
